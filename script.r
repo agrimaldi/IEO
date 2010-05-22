e5da032083abf33cb5724f16d1afee32bb9ab18a
Alexis Grimaldi <sopo.dev@gmail.com>
2010-05-22 21:27:36 +0200

    Added comments + Added DE genes for p53 status alone.

--- script.r
+++ script.r
@@ -55,13 +55,16 @@ plot.ecdf(IQRs, pch=".")
 points(IQRs[probenames],rep(0,length(probenames)),pch=1, col="red")
 abline(v=quantile(IQRs, 0.6), lwd=2, col="orange")
 
+# Non-specific filtering
 filtered_A_batch_RMA <- nsFilter(A_batch_RMA, require.entrez=FALSE, remove.dupEntrez=FALSE, var.func=IQR, var.cutoff=0.6, var.filter=TRUE, filterByQuantile=TRUE, feature.exclude="^AFFX")
 
+# Factorial design
 feset = filtered_A_batch_RMA$eset
 combinedLevels = factor(paste(feset$p53.seq.mut.status..p53..mutant..p53..wt., feset$p53.DLDA.classifier.result..0.wt.like..1.mt.like., sep=""))
 design <- model.matrix(~ 0 + combinedLevels)
 colnames(design) = c("p53m0", "p53m1", "p53p0", "p53p1")
 
+# contrasts : look at differencially expressed genes due to the visible phenotype "p53 mutant-like" in p53-wt tumors, and DE genes due to p53 status in both visible phenotypes mutant-like and wt-like
 cont.matrix <- makeContrasts(
 visible_phenotype_in_p53WT=(p53m1 - p53m0),
 #visible_phenotype_in_p53M=(p53p1 - p53p0),
@@ -78,17 +81,17 @@ library(annotate)
 library(hgu133a.db)
 fit2$genes$Symbol <- getSYMBOL(fit2$genes$ID, "hgu133a")
 
+# ranking of probesets
 topTable(fit2, coef=1, adjust.method="BH", sort.by="P")
 topTable(fit2, coef=2, adjust.method="BH", sort.by="P")
 topTable(fit2, coef=3, adjust.method="BH", sort.by="P")
-topTable(fit2, coef=4, adjust.method="BH", sort.by="P")
 
 results <- decideTests(fit2)
 results
 summary(results)
 
-vennDiagram(results)
 
+# Functional annotation based on the GO annotation
 library(org.Hs.eg.db)
 library(GOstats)
 
@@ -110,7 +113,6 @@ params <- new("GOHyperGParams", geneIds=selectedEntrezGeneIDs,
 paramsCond <- params
 conditional(paramsCond) <- TRUE
 hgOverA <- hyperGTest(paramsCond)
-#hgOverA <- hyperGTest(params)
 
 selectedEntrezGeneIDs = unlist(mget(Mlike_p53WT_DE, hgu133aENTREZID))
 params <- new("GOHyperGParams", geneIds=selectedEntrezGeneIDs,
@@ -120,13 +122,44 @@ params <- new("GOHyperGParams", geneIds=selectedEntrezGeneIDs,
 paramsCond <- params
 conditional(paramsCond) <- TRUE
 hgOverB <- hyperGTest(paramsCond)
-#hgOverB <- hyperGTest(params)
 
-#selectedEntrezGeneID = unlist(mget(p53_status_DE, hgu133aENTREZID))
-#params <- new("GOHyperGParams", geneIds=selectedEntrezGeneIDs,
-#                universeGeneIds=entrezGeneUniverse, annotation="hgu133a.db",
-#                ontology="BP", pvalueCutoff=0.05, conditional=FALSE,
-#                testDirection="over")
+summary(hgOverA)
+summary(hgOverB)
 
-head(summary(hgOverA))
-head(summary(hgOverB))
+#############################################################
+# Look at DE genes due only to the p53 status of the tumors
+
+design = model.matrix(~ 0 + feset$p53.seq.mut.status..p53..mutant..p53..wt.)
+
+colnames(design)= c("WT", "M")
+cont.matrix = makeContrasts(WTvsM = (M - WT), levels = design)
+cont.matrix
+
+fit <- lmFit(feset, design)
+
+fit2  <- contrasts.fit(fit, cont.matrix)
+fit2  <- eBayes(fit2)
+
+library(annotate)
+library(hgu133a.db)
+fit2$genes$Symbol <- getSYMBOL(fit2$genes$ID, "hgu133a")
+
+topTable(fit2, coef=1, adjust.method="BH", sort.by="P")
+
+results <- decideTests(fit2)
+results
+summary(results)
+
+affyUniverse = featureNames(feset)
+entrezGeneUniverse <- unlist(mget(affyUniverse, hgu133aENTREZID))
+DE_WTvsM = rownames(results[results[, "WTvsM"]!=0, ])
+selectedEntrezGeneIDs = unlist(mget(DE_WTvsM, hgu133aENTREZID))
+params <- new("GOHyperGParams", geneIds=selectedEntrezGeneIDs,
+                 universeGeneIds=entrezGeneUniverse, annotation="hgu133a.db",
+                 ontology="BP", pvalueCutoff=0.05, conditional=FALSE,
+                 testDirection="over")
+
+paramsCond <- params
+conditional(paramsCond) <- TRUE
+hgOverA <- hyperGTest(paramsCond)
+summary(hgOverA)
